0bd91c5ac2d3ea8377b991eb2e8e904dc2c894b8
CoderXYZ14 <s1404islam@gmail.com>
2024-04-22 03:16:28 +0530

    L5

---
+++ L5/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name  string = "Shahwaiz"
+	var version = 76
+	fmt.Println(name)
+	fmt.Println(version)
+
+	var money int=6700
+	var currency=3400
+	fmt.Println(money,"    ",currency)
+
+	var dimension float64=87.12
+	fmt.Println(dimension)
+
+	var decided bool=false
+	fmt.Println(decided)
+	
+	const pi=3.14
+	fmt.Println(pi)
+}
